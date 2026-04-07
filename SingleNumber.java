4985009fb34f5b2b988cbf7e32e9f20e4f4b4427
sanket-n-sonawane <93423082+sanket-n-sonawane@users.noreply.github.com>
2026-04-07 11:25:03 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Single number is: " + singleNumber(nums));
+        sc.close();
+    }
+
+    public static int singleNumber(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            int count = 0;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
